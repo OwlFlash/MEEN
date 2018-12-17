95db122c78f3aac4ac597cd3c45fc143c3b40f81
Jakub Zawiślak <jakubatub@gmail.com>
2018-12-17 13:24:44 +0100

    Added two views (when login and when logged) , npm install ejs, added route to ejs files

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
+// Load view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+
+// Static files
+app.use(express.static('./public'));
+
+// Login route
 app.get('/', function(req, res){
-    res.send("Hello Node.js");
+    res.render("login");
 });
+// After logged route
+app.get('/logged', function(req, res){
+    res.render("logged");
+})
+// Login route
 
 app.listen(3000);
 console.log("Listening on port 3000");
