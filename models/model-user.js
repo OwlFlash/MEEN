4e67ca19ad6981ac21aa8d260a3941ab50ba1ded
jakub andrzejewski <baroshem.b@gmail.com>
2018-12-22 21:05:02 +0100

    fixed problems

--- models/model-user.js
+++ models/model-user.js
@@ -1,12 +1,19 @@
 const Joi = require('joi');
 const mongoose = require ('mongoose');
+const bcrypt = require('bcrypt');
 
-const User = mongoose.model('User', new mongoose.Schema({
-    name: {
+var UserSchema = new mongoose.Schema({
+    username: {
         type: String,
         required: true,
         minlength: 5,
         maxlength: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024
     },
     password: {
         type: String,
@@ -15,16 +22,43 @@ const User = mongoose.model('User', new mongoose.Schema({
         maxlength: 1024
     }
     
-}));
+});
 
-function validateUser(user) {
-    const schema = {
-        name: Joi.string().min(5).max(50).required(),
-        password: Joi.string().min(5).max(255).required(),
-    };
-
-    return Joi.validate(user, schema);
+//authenticate input against database
+UserSchema.statics.authenticate = function (email, password, callback) {
+    User.findOne({
+            email: email
+        })
+        .exec(function (err, user) {
+            if (err) {
+                return callback(err)
+            } else if (!user) {
+                var err = new Error('User not found.');
+                err.status = 401;
+                return callback(err);
+            }
+            bcrypt.compare(password, user.password, function (err, result) {
+                if (result === true) {
+                    return callback(null, user);
+                } else {
+                    return callback();
+                }
+            })
+        });
 }
 
-exports.User = User;
-exports.validate = validateUser;
+//hashing a password before saving it to the database
+UserSchema.pre('save', function (next) {
+    var user = this;
+    bcrypt.hash(user.password, 10, function (err, hash) {
+        if (err) {
+            return next(err);
+        }
+        user.password = hash;
+        next();
+    })
+});
+
+
+var User = mongoose.model('User', UserSchema);
+module.exports = User;
